2d69b6b2936139d8dc38a08b1cdd21f631fa42ac
arangurenalonso <69709834+arangurenalonso@users.noreply.github.com>
2025-05-11 00:02:29 -0700

    Feature/MigracionFormSection

--- src/Modules/Survey/03-Infrastructure/QuickForm.Modules.Survey.Persistence/Repositories/SurveyRepository.cs
+++ src/Modules/Survey/03-Infrastructure/QuickForm.Modules.Survey.Persistence/Repositories/SurveyRepository.cs
@@ -1,8 +1,10 @@
 ﻿using QuickForm.Common.Domain;
 using QuickForm.Common.Infrastructure;
+using QuickForm.Modules.Survey.Domain;
 
 namespace QuickForm.Modules.Survey.Persistence;
-public class SurveyRepository<TEntity, TEntityId> : RepositoryBase<TEntity, TEntityId>
+public class SurveyRepository<TEntity, TEntityId>
+    : RepositoryBase<TEntity, TEntityId>, ISurveyRepository<TEntity, TEntityId>
     where TEntity : BaseDomainEntity<TEntityId>
     where TEntityId : EntityId
 {
