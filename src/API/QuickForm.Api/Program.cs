77c125fc0aa50c5ff2ed1ed217ce91eedbb7ca09
arangurenalonso <69709834+arangurenalonso@users.noreply.github.com>
2026-03-11 23:21:38 -0700

    AlwaysShowSwagger

--- src/API/QuickForm.Api/Program.cs
+++ src/API/QuickForm.Api/Program.cs
@@ -65,10 +65,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.ApplyMigrations();
 }
 
